d613f1fc54c0c097e055ce33eaa7c8b47e221b77
Ednei Romão Gomes Dantas <edgomes20@hotmail.com>
2024-01-23 11:48:01 -0300

    Aula 03

---
+++ tipo-boolean.js
@@ -0,0 +1,14 @@
+const estudante = 'Fernando';
+const aprovado = true;
+
+if (aprovado) {
+    console.log('Parabéns, boas festas');
+} else {
+    console.log('Reprovado, boas festas');
+}
+
+if (estudante === 'Fernando') {
+    console.log(`Olá ${estudante}`);
+} else {
+    console.log('Quem é você?');
+}
---
+++ tipo-null-undefined.js
@@ -0,0 +1,13 @@
+let nomeEstudante;
+const professora = 'Ana';
+let telefoneEstudante = null;
+
+console.log(nomeEstudante);
+console.log(typeof nomeEstudante);
+console.log(typeof professora);
+console.log(typeof telefoneEstudante);
+
+
+console.log(telefoneEstudante + 3);
+
+console.log(nomeEstudante + 3);
