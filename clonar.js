7886cbb52026ea5c7266a2f17d1133b50921d198
Ednei Romão Gomes Dantas <edgomes20@hotmail.com>
2024-01-25 11:54:22 -0300

    Aula 05

---
+++ clonar.js
@@ -0,0 +1,16 @@
+const notas = [7, 7, 8, 9];
+const novaListaNotas = notas;
+
+//novaListaNotas.push(10);
+
+// Spread Operator
+const novaListaNotas2 = [...notas, 10];
+
+// let nota = 9.75;
+// let novaNota = nota;
+
+// nota = 10;
+
+console.log(notas);
+console.log(novaListaNotas);
+console.log(novaListaNotas2);
---
+++ funcoes.js
@@ -0,0 +1,9 @@
+//Parâmetros e Agrumentos
+//Retorno
+
+function exibeInfosEstudante (nome, nota) {
+   return console.log(`O nome é ${nome} e a nota é ${nota}`);
+}
+
+exibeInfosEstudante('Caroline', 7);
+exibeInfosEstudante('Ana', 8);
